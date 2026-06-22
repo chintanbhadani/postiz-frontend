88a21651d69ca0e0928d87f127e8d558fecd592f
Chintanb <chintanbhadani2@gmail.com>
2026-06-22 15:39:43 +0530

    Update TikTok verification code to U2SUujrhQDPVUIEKSZDAf3A8O76kxllx

--- app/auth/callback/[platform]/layout.tsx
+++ app/auth/callback/[platform]/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   other: {
-    'tiktok-developers-site-verification': 'D7oiIPLKkIpPyFrF0dP1MjxemnhfkvKB',
+    'tiktok-developers-site-verification': 'U2SUujrhQDPVUIEKSZDAf3A8O76kxllx',
   },
 };
 
