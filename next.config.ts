59011133e034a53c3d1f47ba9ca636526b2057b3
Chintanb <chintanbhadani2@gmail.com>
2026-06-22 14:01:16 +0530

    Added URL

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   turbopack: {},
   // Allow development mode on the VPS IP
-  allowedDevOrigins: ['187.127.171.3', 'localhost', 'reminiscent-ectogenous-amie.ngrok-free.dev'],
+  allowedDevOrigins: ['187.127.171.3', 'localhost', 'f1be-2405-f600-37-12fd-bde1-d65b-fbdb-b410.ngrok-free.app'],
 };
 
 export default nextConfig;
